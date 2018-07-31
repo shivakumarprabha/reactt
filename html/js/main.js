90999ce692ed7352542f47a37d32145e03750034
shivakumarprabha <34454339+shivakumarprabha@users.noreply.github.com>
2018-07-31 11:02:41 +0530

    Add files via upload

---
+++ html/js/main.js
@@ -0,0 +1,1 @@
+console.error("\nE:\\Vimlesh\\MyAssetReact\\main.js:3\nimport Router from './components/Router.jsx';\n^\nParseError: 'import' and 'export' may appear only with 'sourceType: module'");
